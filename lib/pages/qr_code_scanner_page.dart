d4154085008652446889c26271955d9c8cc866a4
Renvle RS <47477363+renvlers@users.noreply.github.com>
2025-04-28 11:45:31 +0800

    added qr scanning

--- lib/pages/qr_code_scanner_page.dart
+++ lib/pages/qr_code_scanner_page.dart
@@ -1,4 +1,7 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend_for_admins/routes/routes.dart';
+import 'package:frontend_for_admins/utils/api_client.dart';
 import 'package:frontend_for_admins/widgets/qr_scanner_overlay.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
@@ -15,8 +18,60 @@ class _QrCodeScannerPageState extends State<QrCodeScannerPage> {
         children: [
           // 相机画面
           MobileScanner(
-            onDetect: (capture) {
+            onDetect: (capture) async {
               // 这里可以处理扫描到的二维码
+              final List<Barcode> barcodes = capture.barcodes;
+              if (barcodes.isNotEmpty) {
+                final String? qrCode = barcodes.first.rawValue;
+                if (qrCode != null) {
+                  // 例如：弹窗显示二维码内容
+                  try {
+                    Response requestResponse = await ApiClient()
+                        .dio
+                        .get("/guest/check_request_by_qr_code?qrCode=$qrCode");
+                    Response ownerResponse = await ApiClient().dio.get(
+                        "/user/get_user?uid=${requestResponse.data['data']['ownerId']}");
+                    DateTime enterTime = DateTime.parse(
+                        requestResponse.data['data']['enterTime']);
+                    DateTime leaveTime = DateTime.parse(
+                        requestResponse.data['data']['leaveTime']);
+                    String guestName =
+                        requestResponse.data['data']['guestName'];
+                    String guestPhone =
+                        requestResponse.data['data']['guestPhone'];
+                    String requestCode =
+                        requestResponse.data['data']['requestCode'];
+                    String qrCodeHash = requestResponse.data['data']['hash'];
+                    String ownerName = ownerResponse.data['data']['username'];
+                    String ownerPhone = ownerResponse.data['data']['phone'];
+                    Navigator.pushNamed(context, RoutePath.detailsPage,
+                        arguments: {
+                          'enterTime': enterTime,
+                          'leaveTime': leaveTime,
+                          'guestName': guestName,
+                          'guestPhone': guestPhone,
+                          'requestCode': requestCode,
+                          'qrCode': qrCodeHash,
+                          'ownerName': ownerName,
+                          'ownerPhone': ownerPhone
+                        });
+                  } on DioException catch (e) {
+                    String errorMessage = e.toString();
+                    if (e.response != null &&
+                        e.response?.data != null &&
+                        e.response?.data['message'] != null) {
+                      errorMessage = e.response?.data['message'];
+                    }
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(errorMessage)),
+                    );
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(e.toString())),
+                    );
+                  }
+                }
+              }
             },
           ),
           // 扫描框遮罩
